2c378c7a2eb6605ffbafb2da7e19276a0af1d836
switer <guankaishe@gmail.com>
2013-08-12 20:54:54 +0800

    add javascript view

---
+++ soft/SFJavascriptView.h
@@ -0,0 +1,13 @@
+//
+//  SFJavascriptView.h
+//  soft
+//
+//  Created by guankaishe on 13-8-12.
+//  Copyright (c) 2013年 guankaishe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFJavascriptView : UIView
+
+@end
---
+++ soft/SFJavascriptView.m
@@ -0,0 +1,31 @@
+//
+//  SFJavascriptView.m
+//  soft
+//
+//  Created by guankaishe on 13-8-12.
+//  Copyright (c) 2013年 guankaishe. All rights reserved.
+//
+
+#import "SFJavascriptView.h"
+
+@implementation SFJavascriptView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
